91e4a4fd9aecaf0a7e09419442d658fe862bc55d
Leticia Monteiro <leticia@vizir.com.br>
2021-06-02 22:12:06 -0300

    feat: implementation storybook v6

--- boilerplate/src/components/Main/index.tsx
+++ boilerplate/src/components/Main/index.tsx
@@ -1,15 +1,21 @@
 import Styled from './styles'
 
-const Main = () => (
+interface Props {
+  title?: string
+  description?: string
+}
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components'
+}: Props) => (
   <Styled.Wrapper>
     <Styled.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
-    <Styled.Title>React Avançado</Styled.Title>
-    <Styled.Description>
-      Typescript, ReactJS, NextJS e Styled Components
-    </Styled.Description>
+    <Styled.Title>{title}</Styled.Title>
+    <Styled.Description>{description}</Styled.Description>
     <Styled.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código"
